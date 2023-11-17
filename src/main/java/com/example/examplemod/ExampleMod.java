fd4d541ef0650cae5ad4dab70fc99c0c83423205
newkincode <maainnewkin59@gmail.com>
2023-11-17 17:43:53 +0900

    Revert "."
    This reverts commit e6325ba0ebd7141008e8b0005dbb74e5f14b7dcd.

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -20,6 +20,7 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
@@ -29,8 +30,8 @@ import net.neoforged.neoforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(com.example.examplemod.NKISDefense.MODID)
-public class NKISDefense
+@Mod(ExampleMod.MODID)
+public class ExampleMod
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -62,7 +63,7 @@ public class NKISDefense
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public NKISDefense(IEventBus modEventBus)
+    public ExampleMod(IEventBus modEventBus)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -81,7 +82,7 @@ public class NKISDefense
         modEventBus.addListener(this::addCreative);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, com.example.examplemod.Config.SPEC);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
@@ -89,12 +90,12 @@ public class NKISDefense
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
 
-        if (com.example.examplemod.Config.logDirtBlock)
+        if (Config.logDirtBlock)
             LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
 
-        LOGGER.info(com.example.examplemod.Config.magicNumberIntroduction + com.example.examplemod.Config.magicNumber);
+        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
 
-        com.example.examplemod.Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
     // Add the example block item to the building blocks tab
