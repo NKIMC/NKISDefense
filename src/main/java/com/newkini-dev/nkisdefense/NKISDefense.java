f76883ecfdcf5ba035d49c868d546a18eabf43fb
newkincode <maainnewkin59@gmail.com>
2023-11-17 18:26:10 +0900

    Revert "Update NKISDefense.java"
    This reverts commit 9773040d9731074dc2646f7089ff6fe24bc91684.

--- src/main/java/com/newkini-dev/nkisdefense/NKISDefense.java
+++ src/main/java/com/newkini-dev/nkisdefense/NKISDefense.java
@@ -1,4 +1,4 @@
-package com.newkini-dev.nkisdefense;
+package com.example.examplemod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -29,7 +29,7 @@ import net.neoforged.neoforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(com.newkini-dev.nkisdefense.NKISDefense.MODID)
+@Mod(com.example.examplemod.NKISDefense.MODID)
 public class NKISDefense
 {
     // Define mod id in a common place for everything to reference
